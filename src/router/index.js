3d6068257bb44a1894f291f32495380bb0038122
nbellana <nbellana@innominds.com>
2023-05-24 11:01:59 +0530

    version-1 completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import QuizesView from "../views/QuizesView.vue"
-import QuizView from "../views/QuizView.vue"
+import QuizesView from "../views/QuizesView.vue"; // Import the QuizesView component
+import QuizView from "../views/QuizView.vue"; // Import the QuizView component
 
+// Create the router instance
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: 'quizes',
-            component: QuizesView
-        },
-        {
-            path: "/quiz/:id",
-            name: "quiz",
-            component: QuizView
-        }
-    ]
-})
+  history: createWebHistory(import.meta.env.BASE_URL), // Set up HTML5 history mode for clean URLs
+  routes: [
+    {
+      path: "/",
+      name: 'quizes',
+      component: QuizesView // Map the root path to the QuizesView component
+    },
+    {
+      path: "/quiz/:id",
+      name: "quiz",
+      component: QuizView // Map the "/quiz/:id" path to the QuizView component
+    }
+  ]
+});
 
-export default router
+export default router; // Export the router instance
